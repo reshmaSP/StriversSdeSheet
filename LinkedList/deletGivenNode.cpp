d54a6b417fd7ab396bedf9a7bf8809c97d2255a6
ReshmaP <panigrahireshma1@gmail.com>
2023-06-07 23:59:49 +0530

    ll

---
+++ LinkedList/deletGivenNode.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* prev=NULL;
+        while(node->next != NULL)
+        {
+            node->val=node->next->val;
+            prev=node;
+            node=node->next;
+        }
+        prev->next=NULL;
+        // delete node;
+    }
+};
+int main()
+{
+}
